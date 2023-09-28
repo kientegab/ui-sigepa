458372e20e19c699ae83bf2b2c3777e226f91dbb
anptic <feloued@ymail.com>
2023-09-28 16:01:07 +0000

    integration parametre structure,type structure,type demande

---
+++ src/app/administration/parametre/type-demande/detail-type-demande/detail-type-demande.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-type-demande',
+  templateUrl: './detail-type-demande.component.html',
+  styleUrls: ['./detail-type-demande.component.scss']
+})
+export class DetailTypeDemandeComponent {
+
+}
