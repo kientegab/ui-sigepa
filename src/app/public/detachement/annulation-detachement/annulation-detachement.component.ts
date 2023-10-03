4282ef44b77acd089b16bf419950c31a067709f8
kientegab <brikientega@gmail.com>
2023-10-03 14:14:55 +0000

    update public page

--- src/app/public/detachement/annulation-detachement/annulation-detachement.component.ts
+++ src/app/public/detachement/annulation-detachement/annulation-detachement.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-annulation-detachement',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./annulation-detachement.component.scss']
 })
 export class AnnulationDetachementComponent {
-
+  constructor(
+    private router: Router
+  ) { }
+  soumettre(){
+    this.router.navigate(['/auth/login']);
+  }
 }
